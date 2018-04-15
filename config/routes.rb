19718f71d301683be720258c75003a6d23437bc5
Johan Faerch <jf@keeptrack.nu>
2018-04-15 18:43:36 +0200

    Rails and Dashing set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  mount Dashing::Engine, at: Dashing.config.engine_path
   devise_for :users
-  root to: 'pages#home'
+  root to: 'dashing/dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
