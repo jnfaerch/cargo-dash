c865dbbc3911e7061330ef59887e68fadbf42187
Johan Faerch <jf@keeptrack.nu>
2018-04-15 21:46:31 +0200

    Widgets and route controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount Dashing::Engine, at: Dashing.config.engine_path
   devise_for :users
-  root to: 'dashing/dashboards#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'dashing/dashboards#index'
+  root to: 'routes#index'
+  resources :routes, only: [ :index, :show ]
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating a user..."
+
+User.destroy_all
+Route.destroy_all
+
+User.create!(email: "johan@faer.ch", password: "123456")
+
+puts "User created"
+
+puts "Creating routes..."
+
+Route.create!(
+  origin: "Gothenburg Port, Sweden",
+  destination: "Shanghai Port, China"
+  )
+
+puts "Finished!"
